8ff13f1b29f5ae2257201bbffb09d6183fcf2792
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2021-09-13 20:49:27 +0300

    Implement navigation

--- lib/src/resources/movie_repository.dart
+++ lib/src/resources/movie_repository.dart
@@ -1,9 +1,13 @@
 import 'dart:async';
 import 'package:movies_app_tmdb/src/models/movie_model.dart';
+import 'package:movies_app_tmdb/src/models/trailer_model.dart';
 import 'movie_api_provider.dart';
 
 class MoviesRepository {
   final moviesApiProvider = MovieApiProvider();
 
   Future<MovieModel> fetchAllMovies() => moviesApiProvider.fetchMovieList();
+
+  Future<TrailerModel> fetchTrailers(int movieId) =>
+      moviesApiProvider.fetchTrailer(movieId);
 }
