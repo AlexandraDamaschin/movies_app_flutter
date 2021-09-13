befda4e6fc31876cb9f4e6cab0f941ca441e81ab
Alexandra Damaschin <damaschin.alexandra.maria@gmail.com>
2021-09-13 20:51:19 +0300

    Navigation (#2)
    * Refactor api call
    
    * Add navigation to movie details screen
    
    * Implement navigation

--- lib/src/resources/movie_api_provider.dart
+++ lib/src/resources/movie_api_provider.dart
@@ -3,20 +3,32 @@ import 'package:http/http.dart' show Client;
 import 'package:movies_app_tmdb/src/models/movie_model.dart';
 import 'dart:convert';
 
+import 'package:movies_app_tmdb/src/models/trailer_model.dart';
+
 class MovieApiProvider {
   Client client = Client();
   final _apiKey = 'af491e18f1d0a6e2955cf1fecc579c1d';
+  final _baseUrl = "http://api.themoviedb.org/3/movie";
 
   Future<MovieModel> fetchMovieList() async {
-    final response = await client.get(Uri.parse(
-        "http://api.themoviedb.org/3/movie/popular?api_key=$_apiKey"));
+    final response =
+        await client.get(Uri.parse("$_baseUrl/popular?api_key=$_apiKey"));
     print(response.body.toString());
     if (response.statusCode == 200) {
-      // If the call to the server was successful, parse the JSON
       return MovieModel.fromJson(json.decode(response.body));
     } else {
-      // If that call was not successful, throw an error.
       throw Exception('Failed to load post');
+    }
+  }
+
+  Future<TrailerModel> fetchTrailer(int movieId) async {
+    final response = await client
+        .get(Uri.parse("$_baseUrl/$movieId/videos?api_key=$_apiKey"));
+
+    if (response.statusCode == 200) {
+      return TrailerModel.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load trailers');
     }
   }
 }
--- lib/src/ui/movie_list.dart
+++ lib/src/ui/movie_list.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:movies_app_tmdb/src/models/movie_model.dart';
 import '../blocs/movies_bloc.dart';
+import 'movie_detail.dart';
 
-class MovieList extends StatelessWidget {
+class MovieList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MovieListState();
+  }
+}
+
+class MovieListState extends State<MovieList> {
+  @override
+  void initState() {
+    super.initState();
+    bloc.fetchAllMovies();
+  }
+
+  @override
+  void dispose() {
+    bloc.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    bloc.fetchAllMovies();
     return Scaffold(
       appBar: AppBar(
         title: Text('Popular Movies'),
@@ -30,10 +49,28 @@ class MovieList extends StatelessWidget {
         gridDelegate:
             new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemBuilder: (BuildContext context, int index) {
-          return Image.network(
-            'https://image.tmdb.org/t/p/w185${snapshot.data!.results[index].posterPath}',
-            fit: BoxFit.cover,
-          );
+          return GridTile(
+              child: InkResponse(
+            enableFeedback: true,
+            child: Image.network(
+              'https://image.tmdb.org/t/p/w185${snapshot.data!.results[index].posterPath}',
+              fit: BoxFit.cover,
+            ),
+            onTap: () => openDetailPage(snapshot.data!, index),
+          ));
         });
+  }
+
+  openDetailPage(MovieModel data, int index) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return MovieDetail(
+        title: data.results[index].title,
+        posterUrl: data.results[index].backdropPath,
+        description: data.results[index].overview,
+        releaseDate: data.results[index].releaseDate.toString(),
+        voteAverage: data.results[index].voteAverage.toString(),
+        movieId: data.results[index].id,
+      );
+    }));
   }
 }
